70afb2a3c9a111841ceac93d900fbaed0d62ae6a
unschlagbar <153451111+unschlagbar@users.noreply.github.com>
2025-12-10 18:16:01 +0100

    gg

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@
 
 include!(concat!(env!("OUT_DIR"), "/gen_assets.rs"));
 
+use std::{net::{Ipv4Addr, SocketAddrV4}, sync::Arc};
+
 use winit::event_loop::EventLoop;
 
 pub mod app;
 mod explorer;
 mod vulkan_render;
+mod network;
 
-use crate::app::App;
+use crate::{app::App, network::Network};
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let mut app = App::create();
+    let network = Arc::new(Network::new(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 2350).into()));
+    let mut app = App::create(network.clone());
+
+    network.connection.lock().unwrap().send(&[1, 2, 4]);
 
     event_loop.run_app(&mut app).unwrap();
+
 }
